1f3c195036b63b264ca6f6baa978444c4a894935
kylerwsm <wsm.kyler@gmail.com>
2019-05-31 15:08:11 +0800

    Changed AppName in main file

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_login_demo/services/authentication.dart';
 import 'package:flutter_login_demo/pages/root_page.dart';
 
+final applicationName = 'KeyManage';
+
 void main() {
   runApp(new MyApp());
 }
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        title: 'Flutter login demo',
+        title: applicationName,
         debugShowCheckedModeBanner: false,
         theme: new ThemeData(
           primarySwatch: Colors.blue,
